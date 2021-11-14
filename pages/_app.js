8a811060d02dfbdae8e1fcb1ede1cd5d5f3c776f
Lucas <lucasalvarez@pm.me>
2021-11-14 16:43:30 -0500

    Add working user accounts and usernames
    Co-authored-by: Noah Nicosia <nnicosia@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import { UserContext } from '../lib/context'
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '../lib/firebase';
+import { userUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const userData = userUserData();
+
+  return (
+    <UserContext.Provider value={{userData}}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import firebase from '../lib/Firebase'
 
-firebase()
+
 
 export default function Home() {
   return (
