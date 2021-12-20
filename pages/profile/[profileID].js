af05d5d548b4c44af85a43ddfc9c6129c1b2c73e
Lucas Alvarez <lucasalvarez@pm.me>
2021-12-19 21:27:31 -0500

    Add working profile page
    Co-authored-by: Noah Nicosia <nnicosia@users.noreply.github.com>

--- pages/profile/[profileID].js
+++ pages/profile/[profileID].js
@@ -5,44 +5,41 @@ import {
   useDocumentData,
   useDocumentOnce,
 } from "react-firebase-hooks/firestore";
-import { firestore, auth, serverTimeStamp } from "../../lib/firebase";
+import {
+  firestore,
+  auth,
+  serverTimeStamp,
+  getUserWithUsername,
+} from "../../lib/firebase";
 import Image from "next/image";
 import { GetUserID } from "../../lib/hooks";
 
-function UserProfile() {
-  // const UID = GetUserID();
-  return (
-    <div>
-      {/* {console.log(`UID`, UID)} */}
-      <ProfilePage />
-    </div>
-  );
+export async function getServerSideProps({ query }) {
+  // const { username } = query;
+  const username = query.profileID;
+
+  const userDoc = await getUserWithUsername(username);
+  let user = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+  }
+  return {
+    props: { user },
+  };
 }
 
-function ProfilePage() {
-  // console.log(`userIDref`, userIDref);
-  // if (userID) {
-  //   const ref = firestore.collection("users").doc(userID);
-  //   const [value, loading, error] = useDocumentData(ref);
-  // }
-  const router = useRouter();
-  const userName = router.query.profileID;
-  const userIDref = firestore.collection("usernames").doc(userName);
-  const [userID, loading, error] = useDocumentData(userIDref);
-  if (!loading) {
-    const ref = firestore.collection("users").doc(userID.uid);
-    const [value, loading, error] = useDocumentData(ref);
-
-    console.log(`ref`, value);
-  }
-  console.log(loading);
+function UserProfile({ user }) {
   return (
-    <>
-      <h1>Profile Page:</h1>
-      {/* <h2>Username: {value && value.username}</h2>
-      <h2>Name: {value && value.displayName}</h2>
-      <img src={value && value.photoURL}></img> */}
-    </>
+    <div>
+      <h1>
+        {user.username.charAt(0).toUpperCase()}
+        {user.username.slice(1)}
+      </h1>
+
+      <img src={user.photoURL} />
+      <p>{user.displayName}</p>
+    </div>
   );
 }
 
