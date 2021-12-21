fe839a250707814a021b382eb522490389ce4907
Lucas Alvarez <lucasalvarez@pm.me>
2021-12-20 22:53:20 -0500

    Add comment posts
    Co-authored-by: Noah Nicosia <nnicosia@users.noreply.github.com>

--- components/TicketView.js
+++ components/TicketView.js
@@ -6,11 +6,13 @@ import {
   useDocumentData,
 } from "react-firebase-hooks/firestore";
 import { withRouter, NextRouter, useRouter } from "next/router";
+import { UserContext } from "../lib/context";
 
 const TicketView = () => {
   return (
     <div>
       <TicketQuery />
+      <CreateNewComment />
     </div>
   );
 };
@@ -46,15 +48,53 @@ function Ticket({ querySnapshot }) {
           <h3>project:{querySnapshot.title}</h3>
           <h3>reporter: {querySnapshot.reporter}</h3>
           <button onClick={() => setEditMode(true)}>Edit mode</button>
+          {/* comments below here */}
         </div>
       ) : (
         <div>
-          <h1> view mode </h1>
+          <h1> edit mode </h1>
 
-          <button onClick={() => setEditMode(false)}>Edit mode</button>
+          <button onClick={() => setEditMode(false)}>View mode</button>
         </div>
       )}
     </>
+  );
+}
+
+// input field and post for user comments
+function CreateNewComment() {
+  const router = useRouter();
+  const [comment, setComment] = useState("");
+  const projectID = router.query.projectID;
+  const ticketID = router.query.id;
+  const { user } = useContext(UserContext);
+  console.log(`user`, user);
+
+  const createComment = async (e) => {
+    e.preventDefault();
+    const ref = firestore
+      .collection("projects")
+      .doc(projectID)
+      .collection("tickets")
+      .doc(ticketID)
+      .collection("comments")
+      .doc();
+
+    const data = {
+      creationDate: serverTimeStamp(),
+      reporter: user.uid,
+      comment,
+    };
+
+    await ref.set(data);
+  };
+
+  return (
+    <form onSubmit={createComment}>
+      <h1>Comment</h1>
+      <input value={comment} onChange={(e) => setComment(e.target.value)} />
+      <button type="submit">Create New Comment</button>
+    </form>
   );
 }
 
