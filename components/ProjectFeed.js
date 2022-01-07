81ee6e1da5957a9231eb0ab0c49854626590bf99
Lucas Alvarez <lucasalvarez@pm.me>
2022-01-07 00:20:03 -0500

    Add MUI, Add Styling
    Co-authored-by: Noah Nicosia <nnicosia@users.noreply.github.com>

--- components/ProjectFeed.js
+++ components/ProjectFeed.js
@@ -1,29 +1,43 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Button, Card, Stack, CssBaseline, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 export default function ProjectFeed({ projects }) {
   return projects
-    ? projects.map((project) => <ProjectItem project={project} key={project.id}/>)
+    ? projects.map((project) => (
+        <ProjectItem project={project} key={project.id} />
+      ))
     : null;
 }
 
 function ProjectItem({ project }) {
   const router = useRouter();
 
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
   return (
-    <>
-      <h1>{project}</h1>
-      <button
+    <Item>
+      <CssBaseline />
+      <Typography variant="h6">{project.data.title}</Typography>
+      <Button
+        variant="outlined"
         type="button"
         onClick={() => {
           router.push({
             pathname: "/projects/[id]/tickets",
-            query: { id: project },
+            query: { id: project.id },
           });
         }}
       >
         View
-      </button>
-    </>
+      </Button>
+    </Item>
   );
 }
